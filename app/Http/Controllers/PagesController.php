3c87e7bcf090e6446f1fc09ad208459789f1258b
Austin.Riley <rileyau@oregonstate.edu>
2017-07-08 18:02:20 -0700

    Working through 'Laravel from Scratch' YouTube series. Through part 4... added Pages controller and a few views  using blade templeting. Added a bootstrap nav bar and fixed up routing.

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('pages.index')->with('title', 'Home');
+    }
+
+    public function about(){
+        return view('pages.about')->with('title', 'About');
+    }
+
+    public function services(){
+        
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+
+        return view('pages.services')->with($data);
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+    <h1>Welcome to Laravel!</h1>
+    <p>This is the Laravel application from the "Laravel from Scratch" YouTube series.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a> <a class="btn btn-success btn-lg" href="#" role="button">Register</a></p> 
+@endsection
